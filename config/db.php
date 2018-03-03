cb517095f9af230e4956b33fcab893f34a8383d6
ifrolikov <frolikov.i@yandex.ru>
2018-03-03 09:50:15 +0300

    fixes

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=smartcoach',
+    'dsn' => 'mysql:host=localhost;dbname=smartcoach_new',
     'username' => 'root',
     'password' => 'ujkjdjyju',
     'charset' => 'utf8mb4',
