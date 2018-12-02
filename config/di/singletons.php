c633c8d268b2c561eb1eea7f1cbfb36376433ef9
frolikov <frolikov@tutu.ru>
2018-12-02 13:35:42 +0300

    di

--- config/di/singletons.php
+++ config/di/singletons.php
@@ -5,14 +5,14 @@ return [
     \app\factories\AbstractConfig::class => \app\models\Config::class,
     \app\factories\AbstractPrice::class => \app\models\Price::class,
     \app\factories\AbstractBlock::class => \app\models\Block::class,
-    \ifrolikov\dto\Interfaces\DtoBuilderInterface::class => function (\Psr\Container\ContainerInterface $container) {
+    \ifrolikov\dto\Interfaces\DtoBuilderInterface::class => function (yii\di\Container $container) {
         return $container->get(\ifrolikov\dto\Facade::class)->getDtoBuilder();
     },
-    \ifrolikov\dto\JsonDtoPacker::class => function (\Psr\Container\ContainerInterface $container) {
+    \ifrolikov\dto\JsonDtoPacker::class => function (yii\di\Container $container) {
         return $container->get(\ifrolikov\dto\Facade::class)->getJsonDtoPacker();
     },
     \ifrolikov\dto\Facade::class => \ifrolikov\dto\Facade::class,
-    \app\repository\SocialNetworks::class => function (\Psr\Container\ContainerInterface $container) {
+    \app\repository\SocialNetworks::class => function (yii\di\Container $container) {
         return new \app\repository\SocialNetworks(
             $container->get(\ifrolikov\dto\JsonDtoPacker::class),
             $container->get(\ifrolikov\dto\Interfaces\DtoBuilderInterface::class)
